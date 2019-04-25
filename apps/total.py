017ddc62b95b3a68479f835c0ee5a9e5814e80d3
8jclxbl <451349242@qq.com>
2019-04-25 20:51:25 +0800

    add student info table

--- apps/total.py
+++ apps/total.py
@@ -1,10 +1,12 @@
 import dash_html_components as html 
 import dash_core_components as dcc
-from dash.dependencies import Input,Output
+from dash.dependencies import Input,Output,State
 from app import app
 
-from models.student import controller_info_by_student_id
+from models.student import controller_info_by_student_id,pd_consumption_by_student_id,get_student_info_by_student_id
 from apps.draw_controller import draw_controller_figure
+from apps.draw_consumption import draw_consumption_figure,draw_consumption_table
+from apps.draw_student import draw_student_table
 
 colors = {     
     'background': '#111111',     
@@ -39,14 +41,31 @@ sub_layout = [
         html.H3(children = '绘图条件', style = {'color':colors['text']}),
         html.Div(id = 'plot-conditions')
     ]),
+    dcc.Tabs(id='graph-table-selector', value = 'graph',children = [
+        dcc.Tab(label = '图', value = 'graph' ),
+        dcc.Tab(label = '数据表', value = 'table'),
+    ]),
+
+    html.Div(id = 'show-block')
+]
+
+student_layout = [
+    html.Div(id = 'student_id',children = [
+        html.H4(id = 'student-id-indicator',children = '请输入所要查询的学号'),
+        dcc.Input(id='input-student-id', type='text', value='13012'),
+        html.Button(children = '提交', id='student-id-submmit',n_clicks = 0), 
+        html.Div(id = 'student-info') 
+    ]),
     html.Div([
-        draw_controller_figure(controller_info_by_student_id(14444))
+        html.H3(children = '绘图条件', style = {'color':colors['text']}),
+        html.Div(id = 'plot-conditions')
     ]),
     dcc.Tabs(id='graph-table-selector', value = 'graph',children = [
         dcc.Tab(label = '图', value = 'graph' ),
         dcc.Tab(label = '数据表', value = 'table'),
-    
-    ])]
+    ]),
+    html.Div(id = 'show-block'),
+]
 
 welcome_layout = [
     dcc.Markdown("""
@@ -65,5 +84,32 @@ welcome_layout = [
 def main_func_selector(value):
     if value == 'welcome':
         return welcome_layout
+    elif value  == 'student_analysis':
+        return student_layout
     else:
         return sub_layout
+
+#注意此处的参数位置和名称无关，只和Input的位置
+@app.callback(
+    Output('student-info', 'children'),
+    [Input('student-id-submmit','n_clicks')],
+    [State('input-student-id', 'value')],
+)
+def select_student(n_clicks,value):
+    try:
+        id = int(value)
+        info = get_student_info_by_student_id(id)
+        return draw_student_table(info)
+    except ValueError:
+        return "学号应该是纯数字"
+
+@app.callback(
+    Output('show-block','children'),
+    [Input('graph-table-selector','value')]
+)
+def graph_table_selector(value):
+    query_res = pd_consumption_by_student_id(14444)
+    if value == 'graph':
+        return draw_consumption_figure(query_res)
+    else:
+        return draw_consumption_table(query_res)
