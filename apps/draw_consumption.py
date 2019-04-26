18acadbd43d5a7775fd2fcbb601708eed31572c2
8jclxbl <451349242@qq.com>
2019-04-26 16:35:17 +0800

    intergrte the consumption draw function

--- apps/draw_consumption.py
+++ apps/draw_consumption.py
@@ -10,9 +10,7 @@ def draw_consumption_graph(query_res):
     x = data['date']
     y = data['money']
     
-
     total = [consumption_graph(x,y,text)]
-
     return dcc.Graph(
             id = 'student-consumption',
             figure = {
@@ -65,7 +63,8 @@ def draw_consumption_table(query_res):
         figure = {
             'data':[go.Table(
                 header = dict(
-                        values = data.columns,
+                        #values = data.columns,
+                        values = ['日期','时间','花费'],
                         line = dict(color='#7D7F80'),
                         fill = dict(color='#a1c3d1'),
                         align = ['left'] * 5),
@@ -81,3 +80,74 @@ def draw_consumption_table(query_res):
             #    height=300)
         }
     )
+
+def consumption_sep_table_graph(query_res,sep,ctype):
+    sumed = consumption_data_seperate(query_res,sep)
+    if ctype == 'graph':
+        return draw_consumption_bar_by_year_month(sumed)
+    else:
+        return draw_consumption_table_by_year_month(sumed)
+
+
+#按月和年份划分
+def consumption_data_seperate(data, sep):
+    info = data['data']
+    info['date'] = pd.to_datetime(info['date'])
+    sumed = info.set_index('date').resample(sep[0])['money'].sum()
+
+    if sep == 'Month':
+        sumed.index = [str(i.year) +'-'+ str(i.month) for i in sumed.index]
+    else:
+        sumed.index = [i.year for i in sumed.index]
+    month_or_year = '年度' if sep =='Year' else '月度'
+    
+    return {'data':sumed,'title_part':month_or_year}
+
+def draw_consumption_table_by_year_month(sumed):
+    data = sumed['data']
+    month_or_year = sumed['title_part']
+
+    return dcc.Graph(
+        id = 'consumption-table-by-year-month',
+        figure = {
+            'data':[go.Table(
+                header = dict(
+                        #values = data.columns,
+                        values = ['时间','花费'],
+                        line = dict(color='#7D7F80'),
+                        fill = dict(color='#a1c3d1'),
+                        align = ['left'] * 5),
+                cells = dict(
+                    values = [data.index, data.values * -1],
+                    line = dict(color='#7D7F80'),
+                    fill = dict(color='#EDFAFF'),
+                    align = ['left'] * 5))
+            ]
+        }
+    )
+
+def draw_consumption_bar_by_year_month(sumed):
+    data = sumed['data']
+    month_or_year = sumed['title_part']
+    total = [
+        go.Bar(
+            x = data.index,
+            y = data.values * -1
+        )
+    ]
+
+    return dcc.Graph(
+            id = 'consumption-graph-by-year-month',
+            figure = {
+                'data':total,
+                'layout': go.Layout(  
+                    autosize=False,     
+                    hovermode='closest',  
+                    dragmode='select',     
+                    title='学生{0}消费统计'.format(month_or_year),
+                    xaxis = dict(title = '时间', showline = True),
+                    yaxis = dict(title = '花费', showline = True),
+                    margin=dict(l=140,r=40,b=50,t=80),
+                )
+            }
+        )
