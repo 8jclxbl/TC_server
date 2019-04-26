357a53edcd5d687a2af14f2112118d28bbcf5c9d
8jclxbl <451349242@qq.com>
2019-04-26 16:37:57 +0800

    add table of teacher of class

--- apps/draw_student.py
+++ apps/draw_student.py
@@ -29,3 +29,28 @@ def draw_student_table(query_res):
 
         }
     )
+
+#class_teachers, 给定id的班级的所有的科目的教师, dict, key = subjectName,vlaue = teacherName
+
+def draw_student_teachers(class_teachers):
+    head = list(class_teachers.keys())
+    value = list(class_teachers.values())
+
+    return dcc.Graph(
+        id = 'student-teachers-table',
+        figure = {
+            'data':[go.Table(
+                header = dict(
+                        values = head,
+                        line = dict(color='#7D7F80'),
+                        fill = dict(color='#a1c3d1'),
+                        align = ['left'] * 5),
+                cells = dict(
+                    values = value,
+                    line = dict(color='#7D7F80'),
+                    fill = dict(color='#EDFAFF'),
+                    align = ['left'] * 5))
+            ],
+
+        }
+    )
