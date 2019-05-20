8123d934aecc6eaa70970f8893b34db50de64eb3
jxqp <451349242@qq.com>
2019-05-20 16:33:46 +0800

    A test of get js from our server.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from models import *
 
 server = Flask(__name__)
 external_stylesheets = ['./static/total.css'] 
-app = dash.Dash(server = server,external_stylesheets=external_stylesheets)
+external_scripts = ['./static/plotly-1.47.0.min.js']
+app = dash.Dash(server = server,external_stylesheets=external_stylesheets,external_scripts=external_scripts)
 app.config.suppress_callback_exceptions = True
 server.config.from_object('config')
--- apps/draw_mass.py
+++ apps/draw_mass.py
@@ -1,6 +1,7 @@
 import dash_html_components as html 
 import dash_core_components as dcc
 import plotly.graph_objs as go 
+
 from apps.simple_chart import dash_table
 
 
